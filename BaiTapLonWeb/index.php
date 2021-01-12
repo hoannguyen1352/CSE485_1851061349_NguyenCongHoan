f5c2da549ffcac43f9574f445b04cc9db2afcfbf
Nguyễn Công Hoan <71828341+hoannguyen1352@users.noreply.github.com>
2021-01-12 18:37:54 +0700

    “[b1]”

---
+++ BaiTapLonWeb/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/47ef22fa15.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- main -->
+    <section id="main">
+        <nav>
+            <!-- logo -->
+            <a href="" class="logo">LoGO</a>
+        <ul class="menu">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a href="#" class="lang">En</a>
+        </nav>
+
+        <!-- name -->
+        <div class="name">
+            <!-- hello -->
+            <p>Hello</p>
+            <!-- name -->
+            
+            <h1>I'm <font color="#17d1ac">HoanNguyen</font> </h1>
+            <!-- details -->
+            <p class="details">I'm a App development.
+                Nice to meet you.Hope having a chance cooperate 
+                <!-- vc-btn -->
+            </p>
+            <a href="#" class="cv-btn">Download CV</a>
+        </div>
+
+        <!-- social -->
+        <div class="social">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter-square"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+        <!-- arrow -->
+        <div class="arrow">
+            
+        </div>
+    </section>
+</body>
+</html>
